870773f8aed38d17d1eb39c4cdf0c1a0853e47bb
itbeien <baintong@aliyun.com>
2025-03-15 17:02:22 +0800

    更新代码

--- ai-labs-master/langchain4j-labs/lab03/src/main/java/cn/itbeien/lab03/ChatMemoryDemo.java
+++ ai-labs-master/langchain4j-labs/lab03/src/main/java/cn/itbeien/lab03/ChatMemoryDemo.java
@@ -1,8 +1,5 @@
 package cn.itbeien.lab03;
 
-import dev.langchain4j.chain.Chain;
-import dev.langchain4j.chain.SimpleChain;
-import dev.langchain4j.model.openai.OpenAiChatModel;
 
 public class ChatMemoryDemo {
 }
