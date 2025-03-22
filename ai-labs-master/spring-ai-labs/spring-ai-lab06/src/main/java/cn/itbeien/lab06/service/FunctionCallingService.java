26eb93e868efcbc560dad3b9544cbb6c72d68734
itbeien <baintong@aliyun.com>
2025-03-22 15:19:25 +0800

    更新代码

--- ai-labs-master/spring-ai-labs/spring-ai-lab06/src/main/java/cn/itbeien/lab06/service/FunctionCallingService.java
+++ ai-labs-master/spring-ai-labs/spring-ai-lab06/src/main/java/cn/itbeien/lab06/service/FunctionCallingService.java
@@ -32,9 +32,14 @@ public class FunctionCallingService {
      */
     @Tool(description = "查询天气")
     public String functionCalling(@ToolParam(description = "哪一天",required = true) String day,@ToolParam(description = "哪个城市",required = true) String city){
-        //调佣查询天气的API接口进行天气查询,查询后返回结果
-        log.info("function calling:{},{}",day,city);
-        return "今天的天气为雾霾";
+        /**
+         * @author itbeien
+         * 项目网站：https://www.itbeien.cn
+         * 比如实现智能天气助手就可以调佣查询天气的API接口进行天气查询,查询后返回结果
+         * 或者在这里调用业务系统功能，然后返回信息让AI大模型给你重新组织语言给到终端用户
+         */
+        log.info("function calling:调用天气查询接口/你的业务系统功能{},{}",day,city);
+        return "今天的天气艳阳高照";
     }
 
 }
