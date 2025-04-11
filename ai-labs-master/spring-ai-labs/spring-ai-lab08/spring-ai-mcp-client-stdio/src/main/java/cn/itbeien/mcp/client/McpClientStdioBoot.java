7ad57754b90cf1693b16bfbc54f45bf00bdb6ce5
itbeien <baintong@aliyun.com>
2025-04-11 20:38:27 +0800

    spring ai mcp stdio

---
+++ ai-labs-master/spring-ai-labs/spring-ai-lab08/spring-ai-mcp-client-stdio/src/main/java/cn/itbeien/mcp/client/McpClientStdioBoot.java
@@ -0,0 +1,19 @@
+package cn.itbeien.mcp.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author itbeien
+ * 项目网站：https://www.itbeien.cn
+ * 公众号：贝恩聊架构
+ * 全网同名，欢迎小伙伴们关注
+ * Java/AI/支付系统/SAAS多租户基础技术平台学习社群
+ * Copyright© 2025 itbeien
+ */
+@SpringBootApplication
+public class McpClientStdioBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(McpClientStdioBoot.class, args);
+    }
+}
---
+++ ai-labs-master/spring-ai-labs/spring-ai-lab08/spring-ai-mcp-client-stdio/src/main/java/cn/itbeien/mcp/client/controller/AiController.java
@@ -0,0 +1,60 @@
+package cn.itbeien.mcp.client.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author itbeien
+ * 项目网站：https://www.itbeien.cn
+ * 公众号：贝恩聊架构
+ * 全网同名，欢迎小伙伴们关注
+ * Java/AI学习社群
+ * Copyright© 2025 itbeien
+ */
+@RestController
+@RequestMapping("/ai")
+@RequiredArgsConstructor
+public class AiController {
+
+    // 自定义人设，来与用户进行角色扮演 提示词
+    private final static String systemPrompt = "你是智能天气助手，请回答与天气相关的问题，用户在询问天气情况时必须要求给出哪个城市和哪一天";
+
+    // 历史消息列表
+    static List<Message> historyMessage = new ArrayList<>(List.of(new SystemMessage(systemPrompt)));
+
+    private final ChatModel chatModel;
+
+    private final ToolCallbackProvider tools;
+
+    // 历史消息列表的最大长度
+    static int maxLen = 1;
+    @GetMapping("/chat")
+    String generation(@RequestParam("prompt") String userInput) {
+        return this.chatModel.call(userInput);
+    }
+
+
+    @GetMapping("/fcChat")
+    String fcChat(@RequestParam("prompt") String userInput) {
+        Prompt prompt = new Prompt(new UserMessage(userInput));
+        return ChatClient.create(chatModel)
+                .prompt(prompt)
+                .system(systemPrompt)
+                .tools(tools)
+                .call()
+                .content();
+    }
+}
